908f2afe5ed939818bf910c48a4eab1541b558d5
Eddy Verbruggen <eddyverbruggen@gmail.com>
2014-08-28 15:05:31 -0500

    implemented demo app

--- Amazon AWS/scripts/demo.js
+++ Amazon AWS/scripts/demo.js
@@ -1,12 +1,5 @@
-// TODO replace these by your own
-var AMAZON_AWS_ACCESSKEYID = "AKIAIDUJXUYFHF6G6S7Q";
-var AMAZON_AWS_SECRETACCESSKEY = "R605uH6CPCdjFILA6nvRd+6IY6oX9ldvGOmBsDt4";
+// replace this by your own
 var AMAZON_AWS_REGION = "us-west-2";
-
-window.onerror = function(a,b,c) {
-  alert(a);
-  alert(c);
-};
 
 (function (global) {
   var DemoViewModel,
@@ -20,42 +13,97 @@ window.onerror = function(a,b,c) {
         bucket.listObjects(function (err, data) {
           if (err) {
             alert(JSON.stringify(err));
-            document.getElementById('status').innerHTML = 'Could not load objects from S3';
           } else {
-            document.getElementById('status').innerHTML = 'Loaded ' + data.Contents.length + ' items from S3';
-            for (var i = 0; i < data.Contents.length; i++) {
-              document.getElementById('objects').innerHTML += '<li>' + data.Contents[i].Key + '</li>';
-            }
+              var result = 'Loaded ' + data.Contents.length + ' items from S3:\n\n';
+              for (var i = 0; i < data.Contents.length; i++) {
+                  result += ' - ' + data.Contents[i].Key + '\n';
+              }
+              alert(result);
           }
         });
       }
     },
 
-    trackButtonClickedWithPayload: function () {
+    listDynamoDBTables: function () {
       if (!this.checkSimulator()) {
-        Leanplum.track(
-            function(msg) {alert(msg)},
-            function(msg) {alert("ERROR, not tracked: " + msg)},
-            "Button",
-            {"amount": 10}
-        );
-      }
-    },
-
-    registerPush: function () {
-      if (!this.checkSimulator()) {
-        Leanplum.registerPush({
-          "badge": "true",
-          "sound": "true",
-          "alert": "true",
-          "callback": "onPushNotificationReceived"
+        var db = new AWS.DynamoDB();
+        db.listTables(function (err, data) {
+          if (err) {
+              alert(JSON.stringify(err));
+              document.getElementById('status').innerHTML = 'Could not load objects from S3';
+          } else {
+              alert(data.TableNames);
+          }
         });
       }
     },
 
-    unregisterPush: function () {
+    updateDynamoDBRecord: function () {
       if (!this.checkSimulator()) {
-        Leanplum.unregisterPush();
+          var table = new AWS.DynamoDB({params: {TableName: 'html5frameworks'}});
+          var keyval = "Kendo UI";
+          var itemParams = {Item: {'Framework ID': {S: keyval}, data: {S: "Last update: " + new Date().toString()}}};
+          table.putItem(itemParams, function(err, data) {
+              if (err) {
+                  alert(err);
+              } else {
+                  alert("Record updated");
+              }
+          });
+      }
+    },
+
+    readDynamoDBRecord: function () {
+      if (!this.checkSimulator()) {
+          var table = new AWS.DynamoDB({params: {TableName: 'html5frameworks'}});
+          var keyval = "Kendo UI";
+          table.getItem({Key: {'Framework ID': {S: keyval}}}, function(err, data) {
+              if (err) {
+                  alert(err);
+              } else {
+                  alert(JSON.stringify(data));
+              }
+          });
+      }
+    },
+
+    addToSQSQueue: function () {
+      if (!this.checkSimulator()) {
+          var sqs = new AWS.SQS();
+          // create a queue if necessary
+          sqs.createQueue({QueueName: 'Telerik_Amazon_AWS_Demo_Queue'}, function (err, data) {
+              if (err) {
+                  alert(err);
+              } else {
+                  sqsQueue = data.QueueUrl;
+                  // add a message to the queue
+		          var queue = new AWS.SQS({params: {QueueUrl: sqsQueue}});
+                  queue.sendMessage({MessageBody: 'Hey there @ ' + new Date().toString()}, function (err, data) {
+                      if (err) {
+                          alert(err);
+                      } else {
+		                  alert("Added message to the queue");
+                      }
+                  });
+              }
+          });
+      }
+    },
+
+    readFromSQSQueue: function () {
+      if (!this.checkSimulator()) {
+          if (sqsQueue == null) {
+              alert("This demo app expects you to add something to the queue first, sorry");
+              return false;
+          }
+          var queue = new AWS.SQS({params: {QueueUrl: sqsQueue}});
+          queue.receiveMessage(function (err, data) {
+              if (err) {
+                  alert(err);
+              } else {
+                  alert(JSON.stringify(data.Messages));
+              }
+          });
       }
     },
 
@@ -78,12 +126,10 @@ window.onerror = function(a,b,c) {
   };
 })(window);
 
+var sqsQueue = null;
+
 document.addEventListener("deviceready", function () {
   if (window.AWS !== undefined) {
-    AWS.config.update({
-      accessKeyId: AMAZON_AWS_ACCESSKEYID,
-      secretAccessKey: AMAZON_AWS_SECRETACCESSKEY
-    });
     AWS.config.region = AMAZON_AWS_REGION;
   }
 });
